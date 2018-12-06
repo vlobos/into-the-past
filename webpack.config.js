d9e2e8dfb2435f5d72f75757546217477371ce31
vlobos <suryvlobos@gmail.com>
2018-12-05 16:02:28 -0800

    Initial webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const ENTRY_DIR = path.join(__dirname, "/client");
+const OUTPUT_DIR = path.join(__dirname, "/public");
+
+module.exports = {
+  entry:`${ENTRY_DIR}/index.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: OUTPUT_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+};
